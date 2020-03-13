89107af1200bcb16741b56c77cf763c13ddf1400
Barrett Helms <barrett.helms@salesloft.com>
2020-03-13 15:54:48 -0400

    Changes github action to only run on PR merge
    * Forces SSL for Gigalixir

--- config/releases.exs
+++ config/releases.exs
@@ -3,4 +3,5 @@ import Config
 config :super_perfundo, SuperPerfundoWeb.Endpoint,
   server: true,
   http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
