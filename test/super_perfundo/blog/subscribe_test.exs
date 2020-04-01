e5702ae1f730bce6733c4ff0058faa48b0c2d4a6
Barrett Helms <barrett.helms@salesloft.com>
2020-04-01 07:44:05 -0400

    Adds email verification step

--- test/super_perfundo/blog/subscribe_test.exs
+++ test/super_perfundo/blog/subscribe_test.exs
@@ -2,6 +2,22 @@ defmodule SuperPerfundo.Blog.SubscribeTest do
   use ExUnit.Case
   alias SuperPerfundo.Blog.Subscribe
 
-  test "verify_email/1" do
+  describe "verify_email/1" do
+    test "returns :ok when given valid email address" do
+      assert {:ok, _} = Subscribe.verify_email("bob@awesome.io")
+    end
+
+    test "return :error when given an invalid email address" do
+      assert :error = Subscribe.verify_email("bob")
+      assert :error = Subscribe.verify_email("bob@")
+      assert :error = Subscribe.verify_email("bob@gmail")
+      assert :error = Subscribe.verify_email("@gmail.com")
+      assert :error = Subscribe.verify_email("bob@gmail.")
+    end
+  end
+
+  describe "persist/1" do
+    test "email is stored in AWS S3" do
+    end
   end
 end
