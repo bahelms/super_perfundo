74f766811dbf39f18a7b46098985074e794585f4
bahelms <jimbonkgit@gmail.com>
2020-04-23 07:21:21 -0400

    Changes host name for prod

--- config/releases.exs
+++ config/releases.exs
@@ -3,7 +3,7 @@ import Config
 config :super_perfundo, SuperPerfundoWeb.Endpoint,
   server: true,
   http: [port: {:system, "PORT"}],
-  url: [host: System.get_env("APP_NAME") <> ".gigalixirapp.com", port: 443]
+  url: [host: System.get_env("HOST")]
 
 config :super_perfundo, SuperPerfundo.Mailer,
   adapter: Bamboo.SendGridAdapter,
