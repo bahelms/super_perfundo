77d12ad42be566db6352af9662dfcfcfa7148bbb
bahelms <jimbonkgit@gmail.com>
2020-07-03 00:00:26 -0400

    Finishes all cube piece designs

--- lib/super_perfundo_web/live/quarto_live.ex
+++ lib/super_perfundo_web/live/quarto_live.ex
@@ -25,14 +25,72 @@ defmodule SuperPerfundoWeb.QuartoLive do
              <div class="side right-tall light"></div>
            </div>
         </div>
-        <div class="slot"></div>
-        <div class="slot"></div>
+        <div class="slot">
+           <div class="cube">
+             <div class="side front-tall dark"></div>
+             <div class="side back-tall dark"></div>
+             <div class="side top-tall dark"></div>
+             <div class="side bottom-tall dark"></div>
+             <div class="side left-tall dark"></div>
+             <div class="side right-tall dark"></div>
+           </div>
+        </div>
+        <div class="slot">
+           <div class="cube">
+             <div class="side front-short light"></div>
+             <div class="side back-short light"></div>
+             <div class="side top-short light"></div>
+             <div class="side bottom-short light"></div>
+             <div class="side left-short light"></div>
+             <div class="side right-short light"></div>
+           </div>
+        </div>
       </div>
       <div class="row">
-        <div class="slot"></div>
-        <div class="slot"></div>
-        <div class="slot"></div>
-        <div class="slot"></div>
+        <div class="slot">
+           <div class="cube">
+             <div class="side front-short dark"></div>
+             <div class="side back-short dark"></div>
+             <div class="side top-short dark"></div>
+             <div class="side bottom-short dark"></div>
+             <div class="side left-short dark"></div>
+             <div class="side right-short dark"></div>
+             <div class="hollow hollow-short"></div>
+           </div>
+        </div>
+        <div class="slot">
+           <div class="cube">
+             <div class="side front-tall light"></div>
+             <div class="side back-tall light"></div>
+             <div class="side top-tall light"></div>
+             <div class="side bottom-tall light"></div>
+             <div class="side left-tall light"></div>
+             <div class="side right-tall light"></div>
+             <div class="hollow hollow-tall"></div>
+           </div>
+        </div>
+        <div class="slot">
+           <div class="cube">
+             <div class="side front-tall dark"></div>
+             <div class="side back-tall dark"></div>
+             <div class="side top-tall dark"></div>
+             <div class="side bottom-tall dark"></div>
+             <div class="side left-tall dark"></div>
+             <div class="side right-tall dark"></div>
+             <div class="hollow hollow-tall"></div>
+           </div>
+        </div>
+        <div class="slot">
+           <div class="cube">
+             <div class="side front-short light"></div>
+             <div class="side back-short light"></div>
+             <div class="side top-short light"></div>
+             <div class="side bottom-short light"></div>
+             <div class="side left-short light"></div>
+             <div class="side right-short light"></div>
+             <div class="hollow hollow-short"></div>
+           </div>
+        </div>
       </div>
       <div class="row">
         <div class="slot"></div>
