6c2d6784c8622bec06d614e5bbd60ea5ff2f95af
bahelms <jimbonkgit@gmail.com>
2022-06-28 15:23:06 -0400

    Final update before draft published

--- test/super_perfundo/quarto/ai_test.exs
+++ test/super_perfundo/quarto/ai_test.exs
@@ -7,6 +7,7 @@ defmodule SuperPerfundo.Quarto.AITest do
       board = {nil, nil, 8, nil}
       {position, _piece} = AI.choose_position_and_next_piece(board, 10)
       assert position >= 0 && position < tuple_size(board)
+      refute position == 2
     end
 
     test "an integer representing another piece is returned" do
